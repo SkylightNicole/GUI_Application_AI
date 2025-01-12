2739af7b199006ec7420abe5368118a4fbd6023e
ChloeyChan <jjnakon999@gmail.com>
2025-01-12 23:26:43 +0700

    Change from local DB to hosting website.

--- Data_Base.py
+++ Data_Base.py
@@ -1,72 +1,126 @@
-import sqlite3 as sql
 import hashlib
 import os
+from dotenv import load_dotenv
+import mysql.connector as mysql
 
-Database = "Database.db"
+load_dotenv()
+DB_Password = os.getenv("DB_Password")
 
 def Write_Register(Username,Password):
     """Salt and Write it down on Database"""
+    connection = None
     salt = os.urandom(10)
     hash_pass = hashlib.pbkdf2_hmac("sha256",Password.encode("utf-8"),salt,50000)
     salt = salt.hex()
     hash_pass = hash_pass.hex()
     try:
-        Connect = sql.connect(Database)
-        Connect.execute("INSERT INTO Login_Info VALUES(?,?,?);",(Username,hash_pass,salt))
-        Connect.commit()
+        connection = mysql.connect(
+            host = "sql12.freesqldatabase.com",
+            user = "sql12756667",
+            password = DB_Password,
+            database = "sql12756667"
+        )
+        if connection is not None and connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO Login_Information VALUES(%s,%s,%s);",(Username,hash_pass,salt))
+            connection.commit()
     except Exception as w:
         print("Error : ",w)
+    except mysql.Error as s:
+        print("MySQL Error : ",s)
     finally:
-        Connect.close()
-        print("Connection Closed Successfully")
+        if connection is not None and connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("Connection Closed Successfully")
 
 def Check(Username,Password):
     """Use to check whether Username nad Password is correct"""
-    check = 0
-    Connect = sql.connect(Database)
-    Info = Connect.execute("SELECT * FROM Login_Info")
-    for i in Info:
-        if i[0] == Username:
-            Pass_bi = bytes.fromhex(i[1])
-            Salt_bi = bytes.fromhex(i[2])
-            Veri_Pass = hashlib.pbkdf2_hmac("sha256",Password.encode("utf-8"),Salt_bi,50000)
-            if Pass_bi == Veri_Pass:
-                print("Correct!")
-                Connect.close()
-                return True
-            else:
-                print("Incorrect Password")
-                Connect.close()
-                return False
-    Connect.close()
-    return False
+    connection = None
+    try:
+        check = 0
+        connection = mysql.connect(
+            host = "sql12.freesqldatabase.com",
+            user = "sql12756667",
+            password = DB_Password,
+            database = "sql12756667"
+        )
+        if connection is not None and connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM Login_Information")
+            Info = cursor.fetchall()
+            for i in Info:
+                if i[0] == Username:
+                    Pass_bi = bytes.fromhex(i[1])
+                    Salt_bi = bytes.fromhex(i[2])
+                    Veri_Pass = hashlib.pbkdf2_hmac("sha256",Password.encode("utf-8"),Salt_bi,50000)
+                    if Pass_bi == Veri_Pass:
+                        print("Correct!")
+                        return True
+                    else:
+                        print("Incorrect Password")
+                        return False
+
+    except Exception as w:
+        print("Error : ",w)
+    except mysql.Error as s:
+        print("MySQL Error : ",s)
+    finally:
+        if connection is not None and connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("Connection Closed Successfully")
 
 def add_more_rider(plate,name,phone,color,brand):
     """Function to add rider to Database"""
+    connection = None
     try:
-        Connect = sql.connect(Database)
-        Connect.execute("INSERT INTO Information VALUES(?,?,?,?,?);",(plate,name,phone,color,brand))
-        Connect.commit()
+        connection = mysql.connect(
+            host = "sql12.freesqldatabase.com",
+            user = "sql12756667",
+            password = DB_Password,
+            database = "sql12756667"
+        )
+        if connection is not None and connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO Information VALUES(%s,%s,%s,%s,%s);",(plate,name,phone,color,brand))
+            connection.commit()
         return True
     except Exception as e:
         print("Error" , e)
         return False
+    except mysql.Error as s:
+        print("MySQL Error : ",s)
     finally:
-        Connect.close()
+        if connection is not None and connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("Connection Closed Successfully")
 
 def rider_check(text):
     """Function to check whether they're rider or not"""
+    connection = None
     try:
-        Connect = sql.connect(Database)
-        Cursor = Connect.cursor()
-        Cursor.execute("SELECT * from Information")
-        result = Cursor.fetchall()
-        for row in result:
-            if row[0] == text:
-                return row
-        return None
+        connection = mysql.connect(
+            host = "sql12.freesqldatabase.com",
+            user = "sql12756667",
+            password = DB_Password,
+            database = "sql12756667"
+        )
+        if connection is not None and connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("SELECT * from Information")
+            result = cursor.fetchall()
+            for row in result:
+                if row[0] == text:
+                    return row
+            return None
     except Exception as s:
         print("Error" , s)
+    except mysql.Error as w:
+        print("MySQL Error : ",w)
     finally:
-        Connect.close()
-    
+        if connection is not None and connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("Connection Closed Successfully")
